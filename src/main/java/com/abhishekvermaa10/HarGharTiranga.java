07893c11df3cfda9d065b209da28a6c7b4718efe
Abhishek Verma <abhishekvermaa10@gmail.com>
2024-08-12 14:49:04 +0530

    Updated

--- src/main/java/com/abhishekvermaa10/HarGharTiranga.java
+++ src/main/java/com/abhishekvermaa10/HarGharTiranga.java
@@ -3,6 +3,10 @@ package com.abhishekvermaa10;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author abhishekvermaa10
+ *
+ */
 @SpringBootApplication
 public class HarGharTiranga {
 
--- src/main/java/com/abhishekvermaa10/controller/HomeController.java
+++ src/main/java/com/abhishekvermaa10/controller/HomeController.java
@@ -3,6 +3,10 @@ package com.abhishekvermaa10.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * @author abhishekvermaa10
+ *
+ */
 @Controller
 public class HomeController {
 	@GetMapping
